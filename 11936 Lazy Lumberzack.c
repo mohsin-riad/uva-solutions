94844520f638f42e080c36a42a2440a59489a5cd
mohsin-riad <45259465+mohsin-riad@users.noreply.github.com>
2019-02-25 19:29:21 +0600

    Add files via upload

---
+++ 11936 Lazy Lumberzack.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d%d",&a,&b,&c);
+        if((a+b)>c && (b+c)>a && (a+c)>b)
+            printf("OK\n");
+        else
+            printf("Wrong!!\n");
+    }
+    return (0);
+}
